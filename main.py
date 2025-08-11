1034c64f74d760d18e14d47e899633cbd22b0d8e
Jyothika Koppula <155452649+JyothikKOPPULA@users.noreply.github.com>
2025-08-11 11:35:44 +0530

    main

--- main.py
+++ main.py
@@ -195,7 +195,7 @@ async def health_check():
 
 if __name__ == "__main__":
     uvicorn.run(
-        "cosmos_main:app",
+        "main:app",
         host="0.0.0.0",
         port=int(os.getenv("PORT", 8000)),
         reload=os.getenv("ENV", "dev") == "dev"
